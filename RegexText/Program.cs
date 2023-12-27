53ff3a0aaff041e9e94d9293af354e4980adff41
Akramjon <144797401+Akramjon009@users.noreply.github.com>
2023-12-27 17:33:41 +0500

    Add project files.

---
+++ RegexText/Program.cs
@@ -0,0 +1,5 @@
+﻿using System.Text.RegularExpressions;
+
+string matn = "AA@%^&CC";
+string yangilanganQator = Regex.Replace(matn, @"[^a-zA-Z0-9_ ]", "");
+Console.WriteLine(yangilanganQator);
---
+++ leetcode10/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.RegularExpressions;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(IsMatch("aa", "a"));
+
+    }
+    public static bool IsMatch(string s, string p)
+    {
+        Regex regex = new Regex($"^{p}$");
+        return regex.IsMatch(s);
+    }
+}
